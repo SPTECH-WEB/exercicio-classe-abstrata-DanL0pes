e9f22df88d4d81f2f9e56da06b1d8332185235eb
DanL0pes <daniel.sousa@sptech.school>
2025-10-31 12:25:16 -0300

    Finalizado! - Daniel Lopes - 04251005

--- src/main/java/school/sptech/Figura.java
+++ src/main/java/school/sptech/Figura.java
@@ -1,4 +1,29 @@
 package school.sptech;
 
-public class Figura {
+public abstract class Figura {
+    private String cor;
+    private Integer espessura;
+
+    abstract public Double calcularArea();
+
+    public String getCor() {
+        return cor;
+    }
+
+    public void setCor(String cor) {
+        this.cor = cor;
+    }
+
+    public Integer getEspessura() {
+        return espessura;
+    }
+
+    public void setEspessura(Integer espessura) {
+        this.espessura = espessura;
+    }
+
+    @Override
+    public  String toString(){
+        return cor+", "+espessura+", "+this.calcularArea();
+    }
 }
--- src/main/java/school/sptech/Imagem.java
+++ src/main/java/school/sptech/Imagem.java
@@ -1,4 +1,44 @@
 package school.sptech;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Imagem {
+    private List<Figura> figuras;
+
+    public Imagem() {
+        this.figuras = new ArrayList<>();
+    }
+
+    public void adicionar(Figura figura){
+        figuras.add(figura);
+    }
+
+    public Double calcularSomaDasAreas(){
+        Double somaAreas = 0.0;
+        for(Figura figura : figuras){
+            somaAreas+= figura.calcularArea();
+        }
+        return somaAreas;
+    }
+
+    public List<Figura> buscarPorAreaMaiorQue20(){
+        List<Figura> figurasMaior20 = new ArrayList<>();
+        for(Figura figura : figuras){
+            if(figura.calcularArea() > 20){
+                figurasMaior20.add(figura);
+            }
+        }
+        return figurasMaior20;
+    }
+
+    public List<Figura> buscarQuadrados(){
+        List<Figura> quadrados = new ArrayList<>();
+        for(Figura figura : figuras){
+            if(figura instanceof Quadrado){
+                quadrados.add(figura);
+            }
+        }
+        return quadrados;
+    }
 }
